22d30254d837a06d44bae981c9beab37097594d1
Lucas Ricarte <lucasrrt@gmail.com>
2018-09-25 22:11:10 -0300

    back and forth movement

--- robot.py
+++ robot.py
@@ -1,9 +1,25 @@
-import serial 
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600) 
+from gpiozero import Servo
+from gpiozero import AngularServo
+from time import sleep
 
-while True: 
-    print ser.readline() 
-    ser.write("180;")
-    ser.write("180;")
-    ser.write("180;")
-    ser.write("false;")
+#ports = [17,27,22,10,9]
+ports = [27,22,10,9]
+servos = []
+
+for port in ports:
+	print "Appending port " + str(port)
+	servos.append(Servo(port))
+
+while True:
+	for index, servo in enumerate(servos, start=0):
+		print "moving servo " + str(ports[index])
+		servo.min()
+		sleep(2)
+	for index, servo in enumerate(servos, start=0):
+		print "moving servo " + str(ports[index])
+		servo.mid()
+		sleep(2)
+	for index, servo in enumerate(servos, start=0):
+		print "moving servo " + str(ports[index])
+		servo.max()
+		sleep(2)
