686b085de094995b2a2444261916bc0ba194e5e9
Lucas Ricarte <lucasrrt@gmail.com>
2018-09-25 23:34:09 -0300

    TCP connection and robot manipulation

--- robot.py
+++ robot.py
@@ -1,9 +1,21 @@
-import serial 
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600) 
+from gpiozero import Servo
+from time import sleep
 
-while True: 
-    print ser.readline() 
-    ser.write("180;")
-    ser.write("180;")
-    ser.write("180;")
-    ser.write("false;")
+#ports = [17, 27, 22, 10, 9]
+ports = [27, 22, 10, 9]
+servos = []
+
+for port in ports: 
+    print "Appending port " + str(port)
+    servos.append(Servo(port))
+
+while True:
+    position = raw_input("prompt")
+    print position
+    position = position.split(" ")
+    print position[0]
+    for index, servo in enumerate(servos, start=0):
+        print "moving servo " + str(ports[index])
+        pos = float(position[index]) / 180 - 1
+        print pos
+        servo.value = pos
