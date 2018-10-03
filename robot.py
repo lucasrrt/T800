b7992546d8d3b7bf72b7c4b8249ad8dba6036b75
Lucas Ricarte <lucasrrt@gmail.com>
2018-10-03 01:00:07 -0300

    robot is server

--- robot.py
+++ robot.py
@@ -1,21 +1,40 @@
-from gpiozero import Servo
 from time import sleep
+import socket
+from gpiozero import Servo
 
+#TCP Connection
+TCP_IP = "192.168.1.9"
+TCP_PORT = 5005
+BUFFER_SIZE = 20
+
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SOCKET.bind((TCP_IP, TCP_PORT))
+
+#Raspberry Connection
 #ports = [17, 27, 22, 10, 9]
-ports = [27, 22, 10, 9]
-servos = []
+PORTS = [27, 22, 10, 9]
+SERVOS = []
 
-for port in ports: 
+for port in PORTS:
     print "Appending port " + str(port)
-    servos.append(Servo(port))
+    SERVOS.append(Servo(port))
 
 while True:
-    position = raw_input("prompt")
+    SOCKET.listen(1) #wait for connection
+    conn, addr = SOCKET.accept()
+    print 'Connection address: ', addr
+
+    position = conn.recv(BUFFER_SIZE)
+    if not position: break
+    print "received data: ", position
+
+    #position = raw_input("prompt")
     print position
-    position = position.split(" ")
-    print position[0]
-    for index, servo in enumerate(servos, start=0):
-        print "moving servo " + str(ports[index])
-        pos = float(position[index]) / 180 - 1
-        print pos
-        servo.value = pos
+    #position = position.split(" ")
+    #print position[0]
+    #for index, servo in enumerate(SERVOS, start=0):
+    #    print "moving servo " + str(PORTS[index])
+    #    pos = float(position[index]) / 180 - 1
+    #    print pos
+    #    servo.value = pos
+    #conn.close()
