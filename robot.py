d2e9f9a4f478cf4c6457ba0b3c3340a18f63e1eb
Lucas Ricarte <lucasrrt@gmail.com>
2018-10-03 00:16:38 -0300

    robot

--- robot.py
+++ robot.py
@@ -1,5 +1,4 @@
 from gpiozero import Servo
-from gpiozero import AngularServo
 from time import sleep
 
 #ports = [17,27,22,10,9]
@@ -7,19 +6,16 @@ ports = [27,22,10,9]
 servos = []
 
 for port in ports:
-	print "Appending port " + str(port)
-	servos.append(Servo(port))
+    print "Appending port " + str(port)
+    servos.append(Servo(port))
 
 while True:
-	for index, servo in enumerate(servos, start=0):
-		print "moving servo " + str(ports[index])
-		servo.min()
-		sleep(2)
-	for index, servo in enumerate(servos, start=0):
-		print "moving servo " + str(ports[index])
-		servo.mid()
-		sleep(2)
-	for index, servo in enumerate(servos, start=0):
-		print "moving servo " + str(ports[index])
-		servo.max()
-		sleep(2)
+    position = raw_input("prompt")
+    print position
+    position = position.split(" ")
+    print position[0]
+    for index, servo in enumerate(servos, start=0):
+        print "moving servo " + str(ports[index])
+        pos = float(position[index]) / 180 - 1
+        print pos
+        servo.value = pos
