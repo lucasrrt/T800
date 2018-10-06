99351e09392bf390a9dd05835653436ba2d4f665
Lucas Ricarte <lucasrrt@gmail.com>
2018-10-06 19:28:49 -0300

    passing angles to robot correctly

--- vision/webcam.py
+++ vision/webcam.py
@@ -3,9 +3,19 @@ import numpy as np
 import ikpy
 from ikpy import plot_utils
 import matplotlib.pyplot as plt
+import socket
+import time
 
+#TCP Connection
+TCP_IP = '192.168.1.9'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "180 180 180 180"
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
 
+#webcam info
 my_chain = ikpy.chain.Chain.from_urdf_file("./poppy_ergo.URDF")
 ax = plot_utils.init_3d_figure()
 
@@ -52,10 +62,26 @@ while(True):
         target_frame = np.eye(4)
         target_frame[:3, 3] = target_vector
 
-        print("The angles of each joints are : ", my_chain.inverse_kinematics(target_frame))
-        ax.cla()
-        my_chain.plot(my_chain.inverse_kinematics(target_frame), ax, target=target_vector)
-        plt.xlim(-0.1, 0.1)
-        plt.ylim(-0.1, 0.1)
-        plt.pause(1.0/33)
+        #print("The angles of each joints are : ", my_chain.inverse_kinematics(target_frame)[1])
+        angles = my_chain.inverse_kinematics(target_frame)
+        string = str(angles[4]) + " " + str(angles[3]) + " " + str(angles[2]) + " " + str(angles[1])
 
+        for i in range(1, len(angles)):
+            if(angles[i] > 0 and angles[i] < 0.0001):
+                angles[i] = 0
+            if(angles[i] < 0 and angles[i] > -0.0001):
+                angles[i] = 0
+
+        print (string.encode())
+
+        s.send(string.encode())
+        time.sleep(1)
+
+
+        #ax.cla()
+        #my_chain.plot(my_chain.inverse_kinematics(target_frame), ax, target=target_vector)
+        #plt.xlim(-0.1, 0.1)
+        #plt.ylim(-0.1, 0.1)
+        # plt.pause(1.0/33)
+data = s.recv(BUFFER_SIZE)
+s.close()
